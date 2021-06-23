98d1a74f4ad63eac08d3d562f51fea927718bdc0
Jeckfeldt <jeckfeldt123@gmail.com>
2021-06-23 13:52:40 -0700

    added outlined html pages to follow

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,14 @@ const router = require('express').Router()
 const passport = require('passport')
 const { join } = require('path')
 
+//login is the main index page (index)
+router.get('/login', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'index.html'))
+})
+
+router.get('/register', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'register.html'))
+})
 
 //export routes
 module.exports = router
