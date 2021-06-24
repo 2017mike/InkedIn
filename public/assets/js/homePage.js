12feb4dd4b5a750df65bff99ab82ebc405714b97
Jeckfeldt <jeckfeldt123@gmail.com>
2021-06-24 14:06:27 -0700

    added render cards

--- public/assets/js/homePage.js
+++ public/assets/js/homePage.js
@@ -8,11 +8,32 @@ const renderPosts = () => {
     }
   })
   .then(({ data }) => {
+    let postType = 'Offer'
     // console.log(data)
     data.forEach(post => {
+      if(postType) {
+        postType = 'Request'
+      }
       //grab each post and render it to page
-      // console.log(post.body)
+      document.getElementById('renderCards').innerHTML += `
+      <div class="col s4">
+        <div class="card">
+          <div class="card-image waves-effect">
+            <img class='activator' src="${post.imgUrl}" alt="image not found">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">${postType}</span>
+            <p><a id="viewPost" href="#">See Post</a></p>
+          </div>
+          <div class="card-reveal">
+            <span' class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+            <img class='image-reveal' src="${post.imgUrl}" alt="image not found">
+          </div>
+        </div>
+      </div>
+      `
       
+      //end forEach
     })
   })
   .catch(err => console.log(err))
