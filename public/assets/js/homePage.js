e5add84ba27df20ee97e6e28fd1504f2a4bb7591
Jeckfeldt <jeckfeldt123@gmail.com>
2021-06-24 11:08:43 -0700

    started renderPosts functionality

--- public/assets/js/homePage.js
+++ public/assets/js/homePage.js
@@ -1,9 +1,25 @@
 
 //render pages
+const renderPosts = () => {
 
+  axios.get('/api/posts', {
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+  })
+  .then(({ data }) => {
+    console.log(data)
+  })
+  .catch(err => console.log(err))
+}
 
 
 
 //listener to view post
 
 
+
+
+
+
+renderPosts()
